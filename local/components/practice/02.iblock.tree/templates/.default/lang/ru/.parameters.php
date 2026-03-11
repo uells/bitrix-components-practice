84f5a2197d383ebe7c5477848a7a6c306de9a5a4
kiteofyte <keruells@gmail.com>
2026-03-11 17:11:03 +0300

    feature(iblock.tree): Созданы описания параметров компонента в ru версии

---
+++ local/components/practice/02.iblock.tree/templates/.default/lang/ru/.parameters.php
@@ -0,0 +1,11 @@
+<?
+$MESS["T_IBLOCK_DESC_LIST_TYPE"] = "Тип информационного блока (используется только для проверки)";
+
+$MESS["ROOT_IBLOCK_ID_DESC"] = "ID корневого информационного блока";
+$MESS["MIDDLE_IBLOCK_ID_DESC"] = "ID промежуточного информационного блока";
+$MESS["TOP_IBLOCK_ID_DESC"] = "ID верхнего информационного блока";
+
+$MESS["ROOT_PROPERTY_CODE_DESC"] = "Свойства корневого информационного блока";
+$MESS["MIDDLE_PROPERTY_CODE_DESC"] = "Свойства промежуточного информационного блока";
+$MESS["TOP_PROPERTY_CODE_DESC"] = "Свойства верхнего информационного блока";
+?>
