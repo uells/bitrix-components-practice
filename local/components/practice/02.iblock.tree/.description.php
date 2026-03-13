0ab429b1bb38c731e88751e5184bed8e78ce004f
kiteofyte <keruells@gmail.com>
2026-03-13 17:25:19 +0300

    feature(iblock.tree): Сконструировано минимальное описание компонента

---
+++ local/components/practice/02.iblock.tree/.description.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    "NAME" => "Список элементов",
+    "DESCRIPTION" => "Выводит список элементов взаимосвязанных инфоблоков",
+    "PATH" => [
+        "ID" => "practice",
+        "NAME" => "Кастомные компоненты [practice]"
+    ]
+]
+?>
